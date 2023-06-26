601ee0281a8ec21dda9cdf74d8a2f389f8c53bbf
Muhammad Hamza <91649627+MuhammadHamza00@users.noreply.github.com>
2023-06-26 12:59:09 +0500

    First Commit

---
+++ motor.js
@@ -0,0 +1,12 @@
+var audio = document.createElement('audio')
+audio.setAttribute('src', 'sound.wav')
+audio.loop = true;
+audio.play();
+var audio = document.createElement('audio')
+audio.setAttribute('src', 'sound2.wav')
+audio.loop = true;
+audio.play();
+var audio = document.createElement('audio')
+audio.setAttribute('src', 'motorvoice.wav')
+audio.loop = true;
+audio.play();
